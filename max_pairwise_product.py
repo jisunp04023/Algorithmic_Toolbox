85bd2c70d17c30342188df0581baf04bf613f727
ferozsun <wltjswltjs7101@gmail.com>
2022-08-08 20:32:17 +0900

    Add files via upload

---
+++ max_pairwise_product.py
@@ -0,0 +1,9 @@
+def max_pairwise_product(numbers):
+    numbers.sort()
+    return numbers[-1]*numbers[-2]
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
